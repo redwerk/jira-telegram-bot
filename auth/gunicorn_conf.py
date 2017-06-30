374796a868760f1b3955acaee12e62403b1a8ead
Dmitry <vasilishin.d.o@gmail.com>
2017-06-30 10:21:40 +0300

    add gunicorn server for flask app

---
+++ auth/gunicorn_conf.py
@@ -0,0 +1,23 @@
+import os
+
+TMP = os.path.expanduser("~/tmp/")
+
+
+def numCPUs():
+    if not hasattr(os, "sysconf"):
+        raise RuntimeError("No sysconf detected.")
+    return os.sysconf("SC_NPROCESSORS_ONLN")
+
+
+port = os.environ.get('GUNICORN_PORT', 9000)
+bind = "127.0.0.1:%s" % port
+workers = numCPUs() + 1
+worker_class = "gevent"
+max_requests = 1000
+daemon = False
+pidfile = TMP + "auth_g.pid"
+accesslog = TMP + "auth_g.access.log"
+errorlog = TMP + "auth_g.error.log"
+graceful_timeout = 60
+timeout = 300
+keepalive = 0
---
+++ auth_run.py
@@ -0,0 +1,5 @@
+# Run auth server.
+from auth.app import app
+
+if __name__ == '__main__':
+    app.run()
