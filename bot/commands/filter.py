baf67921efc65979bfb09ccf0301bc1fba18a9fe
Ihor Peresunko <ihor.peresunko@redwerk.com>
2017-11-25 16:22:26 +0200

    Updated filter commands

--- bot/commands/filter.py
+++ bot/commands/filter.py
@@ -3,7 +3,7 @@ from telegram.ext import CallbackQueryHandler, CommandHandler
 
 from common.utils import build_menu, login_required
 
-from .base import AbstractCommand
+from .base import AbstractCommand, SendMessageFactory
 
 
 class FilterDispatcherCommand(AbstractCommand):
@@ -15,8 +15,6 @@ class FilterDispatcherCommand(AbstractCommand):
         options = kwargs.get('args')
         callback_data = 'filter_p:{}:{}'
         filter_buttons = list()
-        message_type = self.get_message_type(update)
-        result = dict()
 
         filters = self._bot_instance.jira.get_favourite_filters(auth_data=auth_data)
         if options and filters:
@@ -26,7 +24,8 @@ class FilterDispatcherCommand(AbstractCommand):
                 kwargs.update({'filter_name': filter_name, 'filter_id': filters.get(filter_name)})
                 return FilterIssuesCommand(self._bot_instance).handler(bot, update, *args, **kwargs)
             else:
-                result['text'] = 'This filter is not in your favorites'
+                text = 'This filter is not in your favorites'
+                return SendMessageFactory.send(bot, update, text=text, simple_message=True)
         elif filters:
             for name in filters.keys():
                 filter_buttons.append(
@@ -38,12 +37,10 @@ class FilterDispatcherCommand(AbstractCommand):
             )
 
             if buttons:
-                result['text'] = 'Pick up one of the filters:'
-                result['buttons'] = buttons
-        else:
-            result['text'] = "You don't have any favourite filters"
+                text = 'Pick up one of the filters:'
+                return SendMessageFactory.send(bot, update, text=text, buttons=buttons, simple_message=True)
 
-        self.send_factory.send(message_type, bot, update, result, simple_message=True)
+        SendMessageFactory.send(bot, update, text="You don't have any favourite filters", simple_message=True)
 
     def command_callback(self):
         return CommandHandler('filter', self.handler, pass_args=True)
@@ -55,8 +52,6 @@ class FilterIssuesCommand(AbstractCommand):
     @login_required
     def handler(self, bot, update, *args, **kwargs):
         auth_data = kwargs.get('auth_data')
-        message_type = self.get_message_type(update)
-        result = dict()
 
         try:
             scope = self._bot_instance.get_query_scope(update)
@@ -68,12 +63,12 @@ class FilterIssuesCommand(AbstractCommand):
             telegram_id = scope['telegram_id']
             filter_name, filter_id = scope['data'].replace('filter_p:', '').split(':')
 
-        result['title'] = 'All tasks which filtered by «{}»:'.format(filter_name)
-        result['items'] = self._bot_instance.jira.get_filter_issues(
+        title = 'All tasks which filtered by «{}»:'.format(filter_name)
+        items = self._bot_instance.jira.get_filter_issues(
             filter_id=filter_id, filter_name=filter_name, auth_data=auth_data
         )
-        result['key'] = 'filter_p:{}:{}'.format(telegram_id, filter_id)
-        self.send_factory.send(message_type, bot, update, result, items=True)
+        key = 'filter_p:{}:{}'.format(telegram_id, filter_id)
+        SendMessageFactory.send(bot, update, title=title, items=items, key=key)
 
     def command_callback(self):
         return CallbackQueryHandler(self.handler, pattern=r'^filter_p:')
